1d14ee15a4f3fa3dc83d5a914346bc08a7137630
Katie Mendel <katiemendel@Katies-MacBook-Pro.local>
2020-10-12 18:42:36 -0500

    Update dataCollection.py

--- dataCollection.py
+++ dataCollection.py
@@ -6,18 +6,26 @@ import re
 # open file in read mode
 teams = {}
 count = 0
-
+rows = 0
+#games = [row, team1, team2, scoredifference]
+games: {}
 with open('/Users/katiemendel1/Desktop/MarchMadness/mcb2019CSV.csv', 'r') as read_obj:
     # pass the file object to reader() to get the reader object
     csv_reader = reader(read_obj)
     # Iterate over each row in the csv using reader object
     for row in csv_reader:
+        assert(len(row) == 5)
         # row variable is a list that represents a row in csv
         for x in [1, 3]:
+            rows += 1
             #populate dictionary of team names
             team_name = re.sub('@', '', row[x])
             if team_name not in teams.keys():
                 teams[team_name] = count
                 count += 1
+        
 
-print(teams)
+print(rows)
+print(len(teams.values()))
+
+#print(games)
