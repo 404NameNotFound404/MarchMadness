c8059e95fc1de6c6aac6e8ba86ccec500662f337
Katie Mendel <katiemendel@Katies-MacBook-Pro.local>
2020-10-12 13:20:32 -0500

    Update fileCollection prototype

--- fileCollection.py
+++ fileCollection.py
@@ -1,4 +1,3 @@
-import numpy
 import requests
 import re
 from bs4 import BeautifulSoup
@@ -17,6 +16,7 @@ final_array = []
 for i in range(len(mid_array)):
     toAdd = []
     mid_array[i] = mid_array[i].split()
+
     for k in range(1, 5):
         try:
             toAdd.append(mid_array[i][k])
@@ -24,6 +24,25 @@ for i in range(len(mid_array)):
         except:
             #do nothing
             print()
-    final_array.append(toAdd)
+    if toAdd != [] and len(toAdd) == 4:
+        final_array.append(toAdd)
+
+
+
 
 print(final_array)
+
+teams = {}
+count = 0
+
+for i in range(len(final_array)):
+    if final_array[i][0] not in teams.values():
+        teams[count] = final_array[i][0]
+        count += 1
+        
+    if final_array[i][2] not in teams.values():
+        teams[count] = final_array[i][2]
+        count += 1
+
+print(teams)
+    
