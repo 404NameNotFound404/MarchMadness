be5113eeab68793b8cbf3be8206cbc3d36264805
mia-li-z <mli001@csbsju.edu>
2020-10-16 11:58:21 +0800

    add setUp() to testMasseyMethod.py

--- tests/testMasseyMethod.py
+++ tests/testMasseyMethod.py
@@ -1,66 +1,51 @@
 import unittest
 import numpy as np
 
-import MasseyMethod
+import tests.MasseyMethod
 
 class MasseyMethodTest(unittest.TestCase):
+
+
+    def setUp(self):
+        self.a = np.array([[0, 0, 1, 0, -1], [-1, 1, 0, 0, 0], [0, 1, 0, 0, -1], \
+        [-1, 0, 0, 1, 0], [0, 0, -1, 1, 0], [1, 0, 0, 0, -1], \
+        [1, 0, -1, 0, 0], [0, 1, 0, -1, 0], [0, 0, 0, 1, -1], \
+        [0, -1, 1, 0, 0]])
+
+        self.at_a = np.array([[4, -1, -1, -1, -1], \
+        [-1, 4, -1, -1, -1], [-1, -1, 4, -1, -1], \
+        [-1, -1, -1, 4, -1], [1, 1, 1, 1, 1]])
+
+
+        self.at_y = np.array([-40, 32, 15, 103, 0])
+
+        self.y = np.array([32, 8, 25, 49, 14, 7, 10, 2, 42, 7])
+
+        self.results = np.array([-8, 6.4, 3, 20.6, -22])
+
+        self.rankings = ["J", "F", "G", "D", "M"]
+
+        self.teams = {"J": 1, "F": 2, "G": 3, "D": 4, "M": 5}
+
+        self.mas = tests.MasseyMethod.MasseyMethod(self.a, self.y, self.teams)
     
     def testMethodLeftSide(self):
-        a = np.array([[0, 0, 1, 0, -1], [-1, 1, 0, 0, 0], [0, 1, 0, 0, -1], \
-        [-1, 0, 0, 1, 0], [0, 0, -1, 1, 0], [1, 0, 0, 0, -1], \
-        [1, 0, -1, 0, 0], [0, 1, 0, -1, 0], [0, 0, 0, 1, -1], \
-        [0, -1, 1, 0, 0]])
 
-        at_a = np.array([[4, -1, -1, -1, -1], \
-        [-1, 4, -1, -1, -1], [-1, -1, 4, -1, -1], \
-        [-1, -1, -1, 4, -1], [1, 1, 1, 1, 1]])
-
-        y = np.array([32, 8, 25, 49, 14, 7, 10, 2, 42, 7])
-
-        teams = {"J": 1, "F": 2, "G": 3, "D": 4, "M": 5}
-
-        mas = MasseyMethod.MasseyMethod(a, y, teams)
-
-        actual_left_side = mas.methodLeftSide(a)
-        self.assertEqual(actual_left_side.all(), at_a.all())
+        actual_left_side = self.mas.methodLeftSide(self.a)
+        self.assertEqual(actual_left_side.all(), self.at_a.all())
 
     def testMethodRightSide(self):
-        a = np.array([[0, 0, 1, 0, -1], [-1, 1, 0, 0, 0], [0, 1, 0, 0, -1], \
-        [-1, 0, 0, 1, 0], [0, 0, -1, 1, 0], [1, 0, 0, 0, -1], \
-        [1, 0, -1, 0, 0], [0, 1, 0, -1, 0], [0, 0, 0, 1, -1], \
-        [0, -1, 1, 0, 0]])
 
-        at_a = np.array([[4, -1, -1, -1, -1], \
-        [-1, 4, -1, -1, -1], [-1, -1, 4, -1, -1], \
-        [-1, -1, -1, 4, -1], [1, 1, 1, 1, 1]])
-        
-
-        at_y = np.array([-40, 32, 15, 103, 0])
-
-        y = np.array([32, 8, 25, 49, 14, 7, 10, 2, 42, 7])
-
-        results = np.array([-8, 6.4, 3, 20.6, -22])
-    
-        rankings = ["J", "F", "G", "D", "M"]
-
-        teams = {"J": 1, "F": 2, "G": 3, "D": 4, "M": 5}
-        mas = MasseyMethod.MasseyMethod(a, y, teams)
-        actual_right_side = mas.methodRightSide(a, y)
-        self.assertEqual(actual_right_side.all(), at_y.all())
+        actual_right_side = self.mas.methodRightSide(self.a, self.y)
+        self.assertEqual(actual_right_side.all(), self.at_y.all())
 
     def testSetRankings(self):
-        a = np.array([[0, 0, 1, 0, -1], [-1, 1, 0, 0, 0], [0, 1, 0, 0, -1], \
-        [-1, 0, 0, 1, 0], [0, 0, -1, 1, 0], [1, 0, 0, 0, -1], \
-        [1, 0, -1, 0, 0], [0, 1, 0, -1, 0], [0, 0, 0, 1, -1], \
-        [0, -1, 1, 0, 0]])
 
-        y = np.array([32, 8, 25, 49, 14, 7, 10, 2, 42, 7])
-        
         rankings = {"J": 1,"F": 2, "G": 3, "D": 4, "M": 5}
         teams = {"D": 1,"F": 2, "G": 3, "J": 4, "M": 5}
 
-        mas = MasseyMethod.MasseyMethod(a, y, teams)
-        actual_rankings = mas.setRankings(teams, (mas.solve(mas.methodLeftSide(a), mas.methodRightSide(a, y))))
+        # mas = tests.MasseyMethod.MasseyMethod(a, y, teams)
+        actual_rankings = self.mas.setRankings(teams, (self.mas.solve(self.mas.methodLeftSide(self.a), self.mas.methodRightSide(self.a, self.y))))
         #print(actual_rankings)
 
         index1 = 0
@@ -73,18 +58,6 @@ class MasseyMethodTest(unittest.TestCase):
                     self.assertEqual(team1, team2)
 
     def testSolve(self):
-        a = np.array([[0, 0, 1, 0, -1], [-1, 1, 0, 0, 0], [0, 1, 0, 0, -1], \
-        [-1, 0, 0, 1, 0], [0, 0, -1, 1, 0], [1, 0, 0, 0, -1], \
-        [1, 0, -1, 0, 0], [0, 1, 0, -1, 0], [0, 0, 0, 1, -1], \
-        [0, -1, 1, 0, 0]])
-
-        y = np.array([32, 8, 25, 49, 14, 7, 10, 2, 42, 7])
-
-        results = np.array([-8, 6.4, 3, 20.6, -22])
-
-        teams = {"J": 1, "F": 2, "G": 3, "D": 4, "M": 5}
-        mas = MasseyMethod.MasseyMethod(a, y, teams)
-        actual_result = mas.solve(mas.methodLeftSide(a), mas.methodRightSide(a, y))
-        self.assertEqual(actual_result.all(), results.all())
-    
+        actual_result = self.mas.solve(self.mas.methodLeftSide(self.a), self.mas.methodRightSide(self.a, self.y))
+        self.assertEqual(actual_result.all(), self.results.all())
     
